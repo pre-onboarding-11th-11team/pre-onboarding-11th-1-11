e0f4a67cfe877934c3349e17a6478b40a5af0cfe
WONILLISM <wonillism@gmail.com>
2023-06-30 16:01:03 +0900

    [feat] add todo and auth api (#9)

--- src/common/api/auth.ts
+++ src/common/api/auth.ts
@@ -1,3 +1,48 @@
+import axios, { type AxiosResponse } from "axios";
+import { todoAPI } from "./config";
+
+export interface AuthInResponse {
+  status: number;
+  access_token: string;
+}
+
+export const signUp = async (data: {
+  email: string;
+  password: string;
+}): Promise<AxiosResponse<AuthInResponse>> => {
+  try {
+    const response = await todoAPI.post("auth/signup", {
+      email: data.email,
+      password: data.password,
+    });
+    return response;
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      throw e;
+    }
+    throw e;
+  }
+};
+
+export const signIn = async (data: {
+  email: string;
+  password: string;
+}): Promise<AxiosResponse<AuthInResponse>> => {
+  try {
+    const response = await todoAPI.post("auth/signin", {
+      email: data.email,
+      password: data.password,
+    });
+    return response;
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      const { response } = e;
+      throw response?.data?.statusCode;
+    }
+    throw e;
+  }
+};
+
 export const getCurrentUser = () => {
   return localStorage.getItem("access-token");
 };
